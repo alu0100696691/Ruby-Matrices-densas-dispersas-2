2b21ccf2a417c01793f85c36693cc12a9b7442f6
Kevin Robayna <k3vinaso@gmail.com>
2013-12-09 11:26:58 +0000

    Added examples for test DSL

---
+++ lib/suma.rb
@@ -0,0 +1,12 @@
+#Provando DSL de Matrices
+
+#Nueva Operacion Tipo Suma
+operation "suma"
+#Se Muestra por Konsole
+option "console"
+#Mat1 = Dense Matrix
+option "dense" 
+operand [1.0,2.0],[4.0,5.0]
+#Mat2 = Sparse Matrix
+option "sparse"
+operand [2.0,0.0],[4.0,0.0]
